b6cd8fe90437e7e5ddfcc8d537cd9ba7c28857ea
Isaias Malvar <isaiasmg@93hotmail.com>
2024-12-15 17:43:35 +0000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./assets/index.css"
+
+function App() {
+    return (
+        <div className="bg-black text-white flex flex-col ">
+            <span>Talent Acquisition System</span>
+        </div>
+    )
+}
+
+export default App
